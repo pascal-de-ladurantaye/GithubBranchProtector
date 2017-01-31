fe506e13a1d1ebea7937c5fe8d9715de458fa7b3
Pascal de Ladurantaye <pascal.de.ladurantaye@gmail.com>
2017-01-30 23:27:22 -0500

    small fix

--- app/handlers/hook.py
+++ app/handlers/hook.py
@@ -13,6 +13,6 @@ async def hook(request):
     branch = ref.split('/')[-1]
 
     if branch in ALLOWED_BRANCHES:
-        StatusService.set_status('success', 'Commit is on an allowed branch for merging', 'GithubBranchProtector:{}'.format(branch), repo, after_sha)
+        await StatusService.set_status('success', 'Commit is on an allowed branch for merging', 'GithubBranchProtector:{}'.format(branch), repo, after_sha)
 
     return web.Response(text='')
