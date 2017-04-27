f99a7911d678184ee8a4ce38b0963b59c4cb240a
Pascal de Ladurantaye <pascal.de.ladurantaye@gmail.com>
2017-04-27 12:57:14 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from aiohttp import web
 
+from configs import PATH_PREFIX
 from .handlers.hook import hook
 from .handlers.index import get as index
 from .handlers.ping import get as ping
 
 APP = web.Application()
 
-APP.router.add_route('GET', '/', index)
-APP.router.add_route('GET', '/ping', ping)
-APP.router.add_route('POST', '/hook', hook)
+APP.router.add_route('GET', PATH_PREFIX + '/', index)
+APP.router.add_route('GET', PATH_PREFIX + '/ping', ping)
+APP.router.add_route('POST', PATH_PREFIX + '/hook', hook)
